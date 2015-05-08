e26d9c293b0621fb2644e7122ba2f9e1c13cc315
Jenno Ixly <jenno@ixly.in>
2015-05-08 11:35:00 +0530

    Removing index.php from the url add htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -29,7 +29,7 @@ $config['base_url'] = 'http://localhost/wims2/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
