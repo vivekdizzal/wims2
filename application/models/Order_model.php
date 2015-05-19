00ecb9f99c850e06f22c116789d8d4bfbb693550
Jenno Ixly <jenno@ixly.in>
2015-05-19 11:33:46 +0530

    the order model error

--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -12,7 +12,7 @@ class order_model extends CI_Model {
         $this->db->select('*');
         $this->db->join(TBL_CUSTOMER . " tc", 'tj.cust_id = tc.cust_id');
         $this->db->join(TBL_ORDER . " to", 'tj.job_id = to.job_id');
-        $this->db->join(TBL_ORDER_STATUS . " ts", 'tjord_id = ts.ord_id');
+        $this->db->join(TBL_ORDER_STATUS . " ts", 'tj.ord_id = ts.ord_id');
        // $this->db->join(TBL_ORDER_STATUS_UPDATE . " tu", 'tj.job_id = tu.job_id');
         $this->db->where('job_priority', $priority);
         $this->db->where('due_date', $date);
